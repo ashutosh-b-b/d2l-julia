eb67f1a4abec5972df4b95e9e96472386a4d09c6
ashutosh-b-b <a.bharambe123@gmail.com>
2025-03-22 05:50:46 +0000

    d2lai(fit): fix things

--- d2lai/src/fit.jl
+++ d2lai/src/fit.jl
@@ -15,31 +15,56 @@
 #     return losses
 # end
 
-function fit_epoch(model::AbstractClassifier, opt; train_dataloader = nothing, val_dataloader = nothing)
+function clip_gradients! end
+
+function d2lai.clip_gradients!(gs, gradient_clip_val, model)
+    sums = fmap(gs, walk=Flux.Functors.IterateWalk()) do g
+       !isnothing(g) && sum(g.^2)
+   end
+   norm = sqrt(sum(filter(x -> x != false, collect(sums))))
+   g_ = fmap(gs) do d
+       if !isnothing(d)
+           d = d.* (1. / norm)
+       end
+   end
+   g_
+end
+
+function fit_epoch(model::AbstractClassifier, opt; train_dataloader = nothing, val_dataloader = nothing, gradient_clip_val = 0.)
     losses = (train_losses = [], val_losses = [], val_acc = [])
     state = Flux.setup(opt, model)
     for batch in train_dataloader
         gs = gradient(model) do m
             training_step(m, batch)
         end
-        Flux.Optimise.update!(state, model, gs[1])
+        gs = if gradient_clip_val > 0. 
+            clip_gradients!(gs, gradient_clip_val, model)
+        else
+            gs
+        end
+        Flux.update!(state, model, gs[1])
         train_loss = training_step(model, batch)
         push!(losses.train_losses, train_loss)
     end
     for batch in val_dataloader
         loss, acc = validation_step(model, batch)
         push!(losses.val_losses , loss)
-        push!(losses.val_acc, acc)
+        !isnothing(acc) && push!(losses.val_acc, acc)
     end
     return losses
 end
 
-function fit_epoch(model::AbstractModel, opt; train_dataloader = nothing, val_dataloader = nothing)
+function fit_epoch(model::AbstractModel, opt; train_dataloader = nothing, val_dataloader = nothing, gradient_clip_val = 0.)
     losses = (train_losses = [], val_losses = [], val_acc = [])
     state = Flux.setup(opt, model)
     for batch in train_dataloader
         gs = gradient(model) do m
             training_step(m, batch)
+        end
+        gs = if gradient_clip_val > 0. 
+            clip_gradients!(gs, gradient_clip_val, model)
+        else
+            gs
         end
         Flux.Optimise.update!(state, model, gs[1])
         train_loss = training_step(model, batch)
