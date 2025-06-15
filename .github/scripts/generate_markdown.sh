737883cfa1e392707a98b4a5aa49f6f22d78614d
ashutosh-b-b <a.bharambe123@gmail.com>
2025-06-15 23:23:44 +0530

    [ci]: update script

--- .github/scripts/generate_markdown.sh
+++ .github/scripts/generate_markdown.sh
@@ -13,7 +13,7 @@ fi
 PARENT_DIR="$(dirname "$TARGET_DIR")"
 MARKDOWN_DIR="$PARENT_DIR/Julia_Markdown/src"
 mkdir "$MARKDOWN_DIR/img"
-cp -r Julia_Notebooks/img Julia_Markdown/
+cp -r Julia_Notebooks/img Julia_Markdown/src/
 
 for dir in "$TARGET_DIR"/*/; do
     # Check if there are any matches
