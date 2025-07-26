905a21d5ff1f4d9fdc94282974ad58ef4360b323
Ashutosh Bharambe <a.bharambe123@gmail.com>
2025-07-26 10:49:25 +0000

    docs: add CV to make.kl

--- Julia_Markdown/make.jl
+++ Julia_Markdown/make.jl
@@ -21,7 +21,8 @@ chapter_titles = [
     "Modern Convolutional Neural Networks",
     "Recurrent Neural Networks",
     "Modern Recurrent Neural Networks",
-    "Attention Mechanisms and Transformers"
+    "Attention Mechanisms and Transformers",
+    "Computer Vision"
 ]
 
 chapter_folders = [
@@ -34,6 +35,7 @@ chapter_folders = [
  "CH8.Recurrent_Neural_Networks",
  "CH9.Modern_Recurrent_Neural_Networks",
  "CH10.Attention_Mechanisms_and_Transformers",
+ "CH11.Computer_Vision"
 ]
 
 pages = map(chapter_titles, chapter_folders) do title, folder
